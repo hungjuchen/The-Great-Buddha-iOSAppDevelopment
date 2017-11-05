41024bf84b7af980fad4e26378a850e1f0b059b6
Hung-Ju Chen <hongru1015@gmail.com>
2017-11-06 02:58:16 +0800

    Initial Commit

---
+++ The Great Buddha/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  The Great Buddha
+//
+//  Created by Victoria on 2017/11/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
